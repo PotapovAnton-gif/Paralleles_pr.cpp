da26c4faccd483d14f610bb944d16f6181e8b69b
PotapovAnton-gif <antpotapov2019@gmail.com>
2021-02-22 13:35:03 +0300

    data is output to file

--- S_zad_1/Task1.cpp
+++ S_zad_1/Task1.cpp
@@ -1,9 +1,29 @@
-#include <iostraam>
+#include <iostream>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 using namespace std;
 
+int a = 0;
+
+void *mythread(void *value) {
+	pthread_t mythid;
+	mythid = pthread_self();
+	a = a+1;
+	cout << "Thread and result = " << mythid << " " << a << endl;
+	return 0;
+}
+
 int main() {
 	
+	pthread_t thid, mythid;
+
+	int result;
+
+	result = pthread_create(&thid, nullptr, mythid, nullptr);
+	if ()
+
 	return 0;
 	
 }
